d90edcc29c7a14fd602369a91ce26344593a6f6c
Joao <joaoalvesdsx@gmail.com>
2024-07-01 12:41:47 -0300

    img

--- src/components/pages/Followup.jsx
+++ src/components/pages/Followup.jsx
@@ -310,7 +310,7 @@ const FollowUp = () => {
       >
         {currentImage && (
           <>
-            <img src={`https://backend-5eid.onrender.com/get_imagem/${currentImage.path}`} alt={currentImage.descricao} style={{ width: 'auto', height:'auto' }} />
+            <img src={`https://backend-5eid.onrender.com/get_imagem/${currentImage.path}`} alt={currentImage.descricao} style={{ width: '40%', height:'auto' }} />
             <p>{currentImage.descricao}</p>
             <Button  onClick={closeModal}>Fechar</Button>
           </>
