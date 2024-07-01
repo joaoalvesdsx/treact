3b5b51e184b035b451bd62fe702a3e8b20f55f1f
Joao <joaoalvesdsx@gmail.com>
2024-07-01 13:03:09 -0300

    botao menu

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import "../styles/Header.css"
+import React from 'react';
+import "../styles/Header.css";
 import { useNavigate } from 'react-router-dom';
-import Voltar from '../img/back-arrow.png'
+import Voltar from '../img/back-arrow.png';
+import MenuIcon from '../img/menu-principal.png'; // Novo ícone de menu
 
-export const Header = ({text,showBackButton}) => {
+export const Header = ({ text, showBackButton }) => {
   const navigate = useNavigate();
   return (
     <div className="Header">
-       <div className='button-back'>
-       {showBackButton && (
-        <img  src={Voltar} className="back-arrow" onClick={() => navigate(-1)} alt='botao de voltar'>
-        </img>
+      <div className='button-back'>
+        {showBackButton && (
+          <img 
+            src={Voltar} 
+            className="back-arrow" 
+            onClick={() => navigate(-1)} 
+            alt='botao de voltar'
+          />
         )}
-       </div>
+      </div>
       <div className="text-wrapper">{text}</div>
+      <div className='button-menu'>
+        {showBackButton && (
+          <img 
+            src={MenuIcon} 
+            className="menu-icon" 
+            onClick={() => navigate('/menu')} 
+            alt='botao de menu'
+          />
+        )}
+      </div>
     </div>
   );
 };
-export default Header
+
+export default Header;
